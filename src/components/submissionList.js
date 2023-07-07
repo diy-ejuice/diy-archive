2a6093cf35ace2ff8130de142e863c8dc106a752
ayan4m1 <andrew@bulletlogic.com>
2023-07-07 00:28:57 -0400

    get submission list pages working

--- src/components/submissionList.js
+++ src/components/submissionList.js
@@ -1,31 +1,77 @@
 import { Link, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-import { Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  Button,
+  Row,
+  Col,
+  ListGroup,
+  ListGroupItem,
+  Container
+} from 'react-bootstrap';
 
 import Flair from 'components/flair';
 import { getSubmissionUrl } from 'utils';
+import Layout from './layout';
 
-export default function SubmissionList({ data: { submissions } }) {
+export default function SubmissionList({
+  data: {
+    submissions: { nodes: submissions }
+  }
+}) {
+  const pageIndex = parseInt(
+    window.location.href.substring(window.location.href.lastIndexOf('/') + 1),
+    10
+  );
+
   return (
-    <ListGroup>
-      {submissions.map((submission) => (
-        <ListGroupItem key={submission.id}>
-          <Row>
-            <Col xs={1}>
-              <Flair
-                text={submission.linkFlair}
-                color={submission.linkFlairColor}
-              />
-            </Col>
-            <Col xs={11}>
-              <Link to={getSubmissionUrl(submission)}>
-                {submission.title} by {submission.author}
+    <Layout>
+      <Container>
+        <Row>
+          <Col xs={12}>
+            <ListGroup>
+              {submissions.map((submission) => (
+                <ListGroupItem key={submission.id}>
+                  <Row>
+                    <Col xs={1}>
+                      <Flair
+                        text={submission.linkFlair}
+                        color={submission.linkFlairColor}
+                      />
+                    </Col>
+                    <Col xs={11}>
+                      <Link to={getSubmissionUrl(submission)}>
+                        {submission.title} by {submission.author}
+                      </Link>
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+        </Row>
+        <Row className="mt-2">
+          <Col xs={2}>
+            {pageIndex > 1 && (
+              <Link to={`/new/${pageIndex - 1}`}>
+                <Button bg="secondary">
+                  <FontAwesomeIcon icon={faArrowLeft} fixedWidth /> Previous
+                </Button>
               </Link>
-            </Col>
-          </Row>
-        </ListGroupItem>
-      ))}
-    </ListGroup>
+            )}
+          </Col>
+          <Col xs={8}></Col>
+          <Col xs={2} className="text-end">
+            <Link to={`/new/${pageIndex + 1}`}>
+              <Button bg="secondary">
+                <FontAwesomeIcon icon={faArrowRight} fixedWidth /> Next
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
-import { graphql } from 'gatsby';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { Link, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-import { Container, Row, Col } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  ListGroup,
+  ListGroupItem
+} from 'react-bootstrap';
 
 import SEO from 'components/seo';
 import Layout from 'components/layout';
-import SubmissionList from 'components/submissionList';
+import Flair from 'components/flair';
+import { getSubmissionUrl } from 'utils';
 
 export default function IndexPage({ data }) {
   const {
@@ -17,7 +27,32 @@ export default function IndexPage({ data }) {
       <Container>
         <Row>
           <Col xs={12}>
-            <SubmissionList data={{ submissions }} />
+            <ListGroup>
+              {submissions.map((submission) => (
+                <ListGroupItem key={submission.id}>
+                  <Row>
+                    <Col xs={1}>
+                      <Flair
+                        text={submission.linkFlair}
+                        color={submission.linkFlairColor}
+                      />
+                    </Col>
+                    <Col xs={11}>
+                      <Link to={getSubmissionUrl(submission)}>
+                        {submission.title} by {submission.author}
+                      </Link>
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col xs={12} className="d-flex justify-content-end mt-2">
+            <Link to="/new/1">
+              <Button bg="secondary">
+                <FontAwesomeIcon icon={faArrowRight} fixedWidth /> Next
+              </Button>
+            </Link>
           </Col>
         </Row>
       </Container>
